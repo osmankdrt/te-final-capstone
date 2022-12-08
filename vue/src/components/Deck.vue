5fa7ff34e13c7e690868177a871a32da3edb0ac9
Ian West <ianwest2010@gmail.com>
2022-12-08 13:58:13 -0500

    added delete deck frontend functionality

--- vue/src/components/Deck.vue
+++ vue/src/components/Deck.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="decks">
+    
     <router-link :to="{name:'cardView', params: {id: deck.deckID}}" class="deck" tag="div">
+      <div class = "editButtons">
+     <button type="button" class="btn btn-secondary" v-on:click="deleteDeck">❌</button>
+    </div>
      <h3 class="deck-title"> {{deck.deckTitle}} </h3>
      <p class = "description"> {{deck.deckDescription}} </p>
       </router-link>
@@ -10,11 +14,24 @@
 </template>
 
 <script>
+import flashCardService from "../services/FlashCardService.js";
 export default {
  name: 'Deck',
+ deleteDeckID: 0,
  props: {
    deck: Object,
  },
+ methods: {
+   deleteDeck() {
+     this.deleteDeckID = this.deck.deckID
+     flashCardService.deleteDeck(this.deleteDeckID).then(response => {
+       if(response.status === 204) {
+         this.$router.go()
+
+       }
+     }) 
+   }
+ }
 }
 </script>
 
@@ -22,9 +39,7 @@ export default {
 
 .deck{
     
-    justify-content: space-around;
     align-items: center;
-    flex-wrap: wrap;
     border: 5px solid;
     border-color: #D7FFF1;
     border-radius: 10px;
@@ -36,10 +51,14 @@ export default {
     
 }
 
+
+
 p.description{
   display: none;
   padding: 5px;
 }
+
+
 
 div.deck:hover p.description {
   display: block;
@@ -55,7 +74,6 @@ h3.deck-title {
   color: #285943;
   text-shadow: 1px 1px #00000041;
   font-weight: bold;
-  
 
 
 }
