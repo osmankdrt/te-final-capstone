936f0e8e1a4b9eefd248237b1fe7d9f3fb1edf78
Ian West <ianwest2010@gmail.com>
2022-12-08 12:15:18 -0500

    add deck styled and sorts by recently created decks

--- vue/src/components/DeckList.vue
+++ vue/src/components/DeckList.vue
@@ -20,6 +20,7 @@
               fill="currentColor"
               class="bi bi-plus-square"
               viewBox="0 0 16 16"
+              
             >
               <path
                 d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
@@ -30,26 +31,10 @@
             </svg>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="currentColor"
-              class="bi bi-pencil-square"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
               width="30"
               height="30"
               fill="currentColor"
-              class="bi bi-x-square"
+              class="bi bi-x-square delete"
               viewBox="0 0 16 16"
             >
               <path
@@ -64,7 +49,7 @@
           <input
             type="text"
             class="form-control"
-            v-model="newDeck.deckName"
+            v-model="newDeck.deckTitle"
             required
           />
           <label for="deckDescription" class="deckDescription">
@@ -94,7 +79,10 @@ export default {
       id: 0,
       decks: [],
       displayForm: false,
-      newDeck: {},
+      newDeck: {
+        deckTitle: '',
+        deckDescription: ''
+      }
     };
   },
   props: {
@@ -104,9 +92,7 @@ export default {
     Deck,
   },
   computed: {
-    //  filteredDecks() {
-    //      return this.$store.state.decks;
-    //  },
+    
   },
   methods: {
     toggleDisplayForm() {
@@ -115,7 +101,9 @@ export default {
     addDeck() {
       flashCardService.addDecks(this.newDeck).then(response => {
         if(response.status === 201) {
-          this.$router.push('deck')
+          
+          this.$router.go()
+          // this.$router.push(`/decks/${response.data.id}/cards`)
         }
       })
     }
@@ -123,6 +111,7 @@ export default {
   created() {
     flashCardService.listDecks().then((response) => {
       this.decks = response.data;
+      this.decks = this.decks.reverse()
     });
   },
 };
@@ -181,10 +170,9 @@ export default {
 
 .editButtons {
   display: flex;
-  justify-content: space-around;
-  justify-items: flex-end;
-  
+  justify-content: flex-end;
 }
+
 
 .plus {
   /* plus sign on deck shadow */
