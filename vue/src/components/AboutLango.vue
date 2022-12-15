904d15a8738163bdb305abf316062df1f237fb25
Istar Hashi <pearlofjannaah@gmail.com>
2022-12-15 11:27:38 -0500

    pull

--- vue/src/components/AboutLango.vue
+++ vue/src/components/AboutLango.vue
@@ -33,7 +33,7 @@
                 <li class="member-profile prof-1">
                     <h3 class="tm-name">Binyam Tekae</h3>
                     <h4 class="tm-role">Software Developer</h4>
-                    <p class="tm-info">Binyam is the man responsible for most of the graphic design. He is also a famous super hero by night.</p>
+                    <p class="tm-info">Binyam is the man responsible for all of the laughter in our group. Soccer player by day , software developer by night. When not engulfed by the two hobbies binyam loves to take walks on the beach while drinking piña colada and getting caught in the rain. #Shrek</p>
                    <a class="social" href="https://www.linkedin.com/in/binyam-tekae/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
 </svg></a>
@@ -42,7 +42,7 @@
                 <li class="member-profile prof-2">
                     <h3 class="tm-name">Ian West</h3>
                     <h4 class="tm-role">Software Developer</h4>
-                    <p class="tm-info">Ian is a full stack developer based in Columbus, Ohio. When not behind a keyboard, Ian is an avid cyclist and can often be found outdoors with his partner and their dog Pretzel.</p>
+                    <p class="tm-info">Ian is a full stack developer based in Columbus, Ohio. When not behind a keyboard, Ian is an avid cyclist, a fan of reading, and can often be found outdoors with his partner and their dog Pretzel.</p>
                     <a class="social" href="https://www.linkedin.com/in/ianwest614/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
 </svg></a>
@@ -51,7 +51,8 @@
                 <li class="member-profile prof-3">
                     <h3 class="tm-name">Osman Burgazli</h3>
                     <h4 class="tm-role">Software Developer</h4>
-                    <p class="tm-info">Osman is the man responsible for most of the graphic design. He is also a famous super hero by night.</p>
+                    <p class="tm-info">Osman is a quick learner and an investigative problem solver. Osman's life advice is, "We face problems every day, whether ready or not. The only factor we can control is how we respond to them."
+A little fun fact about Osman is that he can play any sport but he is not good at any of them.</p>
                     <a class="social" href="https://www.linkedin.com/in/osmanburgazli/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
 </svg></a>
@@ -60,7 +61,7 @@
                 <li class="member-profile prof-4">
                     <h3 class="tm-name">Istar Hashi</h3>
                     <h4 class="tm-role">Software Developer</h4>
-                    <p class="tm-info">Istar is a woman of many talents but the most current ones are being able to eat a bucket of ice cream in one sitting and also being a full stack software developer.</p>
+                    <p class="tm-info">Istar is a woman of many talents but the most current ones are being able to eat a bucket of ice cream in one sitting and also being a full stack software developer. When not at work Istar is a huge book fanatic. Huge Buckeye fan. #Go Bucks!</p>
                     <a class="social" href="https://www.linkedin.com/in/istar-hashi/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
 </svg></a>
